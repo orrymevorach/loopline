13454ee5efb7887bf58c4eb1954ccc4f06b93e94
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2026-09-19 23:56:50 -0400

    operation information

--- components/HomePage/HomePage.jsx
+++ components/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import Wrapper from '../shared/Wrapper/Wrapper';
 import Carousel from './Carousel/Carousel';
+import HoursAndOperations from './HoursAndOperations/HoursAndOperations';
 
 export default function HomePage({ entries }) {
   const [] = entries;
   return (
     <Wrapper>
       <Carousel />
+      <HoursAndOperations />
     </Wrapper>
   );
 }
